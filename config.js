66a6e60312346ec136f00eda6e482b3cf9380206
anjanaheroku4 <anjanabotheroku@gmail.com>
2024-12-21 00:37:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: 'YOUR SESSION ID',
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'YOUR GIT TOKEN (gph_without)' : process.env.GITHUB_AUTH_TOKEN,
-GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'YOUR GIT USER NAME' : process.env.GITHUB_USER_NAME,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? 'YOUR NUMBER' : process.env.OWNER_NUMBER
+SESSION_ID: 'HANSAMAL-MD=3ZlWGT4B#alWZPL2MZ3v6YqPM7v6iXehCqitHtVc2EepD8Fwk2r4',
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'PZmqhkK2CvAwAd1xwj31QUw16uxkQ91SFoXg' : process.env.GITHUB_AUTH_TOKEN,
+GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'anjanaheroku4' : process.env.GITHUB_USER_NAME,
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94773962796' : process.env.OWNER_NUMBER
 };
